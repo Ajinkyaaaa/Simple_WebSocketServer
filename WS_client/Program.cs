0f86cdc534cb9bf71c198a4676ff67e7ac66861e
Ajinkya Kamble <ajinkyakamble18764@gmail.com>
2024-04-22 18:55:36 +0530

    Simpleserver

---
+++ WS_client/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+var ws = new ClientWebSocket();
+Console.WriteLine("Connecting to server...");
+await ws.ConnectAsync(new Uri("ws://localhost:6969/ws"), CancellationToken.None);
+Console.WriteLine("Connected to webserver")
